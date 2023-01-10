e693864e1a7a5fbe5b4c92ee1f9c27b992b92728
Abbashozefa <abbashozefa@gmail.com>
2023-01-10 23:12:00 +0530

    third

--- ml3.py
+++ ml3.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
+import matplotlib.pyplot as plt 
 
 data=load_wine()
 
@@ -14,7 +15,7 @@ print(data.feature_names)
 print(response_vector)
 feature_name=data.feature_names
 response_vector=data.target_names
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.7,random_state=1)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.8,random_state=1)
 sample =X[0:]
 classifier_knn = KNeighborsClassifier(n_neighbors = 3)
 classifier_knn.fit(X_train, y_train)
@@ -24,3 +25,5 @@ for p in preds:
     pred_species=response_vector[p]
 print(pred_species)
 print("Accuracy:",metrics.accuracy_score(y_test,y_pred))
+plt.scatter(X_test[0:20],X_train[0:20])
+plt.show()
